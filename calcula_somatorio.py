88f214d4bd8bdf8f16d84698f8936b8881e372f7
Francisco Neto <faguiar.eng@gmail.com>
2025-08-29 16:17:03 -0300

    Codigo Enviado

--- calcula_somatorio.py
+++ calcula_somatorio.py
@@ -1,1 +1,13 @@
-# Sua solução aqui
+n = int(input())   # lê n(limite somatório)
+s = 0              # acumula a soma
+
+if n >= 2:
+    # somatório de i = 2 até n(inclusive)
+    for i in range(2, n + 1):
+        s += 1 / (i - 1)
+else:
+    # se n < 2, retorna 0.00
+    s = 0.00
+
+# imprime o resultado
+print(f"{s:.2f}")
